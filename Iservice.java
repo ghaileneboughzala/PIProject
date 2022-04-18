bd72232abe63c49d4a3fc7839b43ce1393273bf7
MAzizBenSalem <mohamedaziz.bensalem@esprit.tn>
2022-04-18 03:12:47 +0100

    aziz
    aziz

---
+++ Iservice.java
@@ -0,0 +1,20 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package services;
+//
+//import java.util.List;
+//
+///**
+// *
+// * @author lenovo
+// */
+//public interface Iservice<T> {  //passer un parametre pour notre interface T:type generique
+//    
+//    public void ajouter(T t);
+//    public void modifier(T t);
+//    //public void supprimer(Object ref);
+//    public List<T> recuperer(); 
+//}
